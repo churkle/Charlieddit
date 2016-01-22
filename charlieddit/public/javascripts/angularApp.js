aae53add9fcb46500b6d96c728b17483bb09b3d8
Charles Fei <charles.fei@vanderbilt.edu>
2016-01-21 21:14:43 -0600

    Added authentication

--- charlieddit/public/javascripts/angularApp.js
+++ charlieddit/public/javascripts/angularApp.js
@@ -28,6 +28,30 @@ app.config([
 	            }
 	        })
 
+	        .state('login', {
+	        	url: '/login',
+	        	templateUrl: '/login.html',
+	        	controller: 'AuthCtrl',
+	        	onEnter: ['$state', 'auth', function($state, auth){
+	        		if(auth.isLoggedIn())
+	        		{
+	        			$state.go('home');
+	        		}
+	        	}]
+	        })
+
+	        .state('register', {
+	        	url: '/register', 
+	        	templateUrl: '/register.html',
+	        	controller: 'AuthCtrl',
+	        	onEnter: ['$state', 'auth', function($state, auth){
+	        		if(auth.isLoggedIn())
+	        		{
+	        			$state.go('home');
+	        		}
+	        	}]
+	        })
+
 	    $urlRouterProvider.otherwise('home');
 	}])
 
@@ -162,4 +186,76 @@ app.factory('posts', ['$http', function($http){
 	};
 
 	return o;
-}])
+}]);
+
+.controller('AuthCtrl', [
+	'$scope',
+	'$state',
+	'auth', 
+	function($scope, $state, auth){
+		$scope.user = {};
+
+		$scope.register = function(){
+			auth.register($scope.user).error(function(error){
+				$scope.error = error;
+			}).then(function(){
+				$state.go('home');
+			});
+		};
+
+		$scope.logIn = function(){
+			auth.logIn($scope.user).error(function(error){
+				$scope.error = error;
+			}).then(function(){
+				$state.go('home');
+			});
+		};
+	}])
+
+.factory('auth', ['$http', '$window', function($http, $window){
+	var auth = {};
+
+	auth.saveToken = function(token){
+		$window.localStorage['charlieddit-token'] = token;
+	};
+
+	auth.getToken = function(){
+		return $window.localStorage['charlieddit-token'];
+	};
+
+	auth.isLoggedIn = function(){
+		var token = auth.getToken();
+
+		if(token)
+		{
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+			return payload.exp > Date.now() / 1000;
+		}
+		else
+		{
+			return false;
+		}
+	};
+
+	auth.currentUser = function(){
+		if(auth.isLoggedIn())
+		{
+			var token = auth.getToken();
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+			return payload.username;
+		}
+	};
+
+	auth.register = function(user){
+		return $http.post('/register', user).success(function(data){
+			auth.saveToken(data.token);
+		});
+	};
+
+	auth.logOut = function(){
+		$window.localStorage.removeItem('charlieddit-token');
+	};
+
+	return auth;
+}]);
